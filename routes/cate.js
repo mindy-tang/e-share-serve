f9b5a4b89918962aa7b2c40af822fb484d3d99e9
mindy_tang <mindy_tang@126.com>
2020-11-19 20:41:45 +0800

    新增分类接口

--- routes/cate.js
+++ routes/cate.js
@@ -3,14 +3,31 @@ const router = express.Router();
 const CateModel = require("../models/cate");
 
 //查询cate全列表
-router.get("/list",(req, res)=>{
-    CateModel.find({},(err, docs) => {
-        if(err){
-            console.log("cate全列表查询失败:",err);
+router.get("/list", (req, res) => {
+    CateModel.find({}, (err, docs) => {
+        if (err) {
+            console.log("cate全列表查询失败:", err);
             return;
         }
         res.send(docs);
     });
 });
+
+//新增cate
+router.post("/add", (req, res) => {
+    const cate = new CateModel({
+        ...req.body,
+        create_time: Date.now()
+    });
+    console.log("新增的cate:", book);
+
+    book.save((err) => {
+        if (err) {
+            console.log("新增cate失败:", err);
+            return;
+        }
+        res.status(200).send("新增成功");
+    });
+})
 
 module.exports = router;
